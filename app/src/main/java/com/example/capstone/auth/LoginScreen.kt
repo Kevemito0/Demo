8363177bf56af4762f665c83c7bdd16e5f2c89e5
Kevem <69041112+Kevemito0@users.noreply.github.com>
2025-04-10 20:33:41 +0300

    Login and register screen

--- app/src/main/java/com/example/capstone/auth/LoginScreen.kt
+++ app/src/main/java/com/example/capstone/auth/LoginScreen.kt
@@ -1,28 +1,20 @@
-package com.example.capstone
+package com.example.capstone.auth
 
-import android.graphics.Paint.Cap
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Password
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -32,15 +24,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.VerticalAlignmentLine
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.capstone.ui.theme.CapstoneTheme
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
@@ -50,17 +36,17 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
-import org.jetbrains.annotations.Nls.Capitalization
-
+import com.example.capstone.AuthScreens
 
 var userName = ""
 
 @Composable
 fun Kerem(paddingValues: PaddingValues) {
 //    MainScreen(paddingValues)
-    LoginScreen(paddingValues)
+    AuthScreens(paddingValues)
 }
 
+/*
 @Composable
 fun MainScreen(paddingValues: PaddingValues) {
     var number by remember { mutableStateOf(0) }
@@ -103,7 +89,7 @@ fun MainScreen(paddingValues: PaddingValues) {
         }
     }
 }
-
+*/
 @Composable
 fun LoginScreen(paddingValues: PaddingValues) {
     var userName by remember { mutableStateOf("") }
@@ -161,10 +147,10 @@ fun LoginScreen(paddingValues: PaddingValues) {
             supportingText = {
                 Text(
                     text = "Forgot Password",
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth().clickable {  },
                     textAlign = TextAlign.End,
                     textDecoration = TextDecoration.Underline,
-                    color = Color.Black
+                    color = Color.Unspecified
                 )
             },
             label = {
@@ -222,7 +208,7 @@ fun LoginScreen(paddingValues: PaddingValues) {
 @Composable
 fun KeremPreview() {
     CapstoneTheme {
-        LoginScreen(PaddingValues());
+        AuthScreens(PaddingValues());
     }
 }
 
