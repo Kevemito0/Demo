e2e537fc1c36b8cc28e1b7a86432929b179c06f3
Poyrazbel <poyrazbel12@gmail.com>
2025-05-01 22:48:36 +0300

    Send for Testing

--- app/src/main/java/com/example/capstone/RoomScreen.kt
+++ app/src/main/java/com/example/capstone/RoomScreen.kt
@@ -1,5 +1,6 @@
 package com.example.capstone
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,6 +17,11 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,11 +31,37 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.capstone.ui.theme.CapstoneTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RoomScreen(navController: NavController, paddingValues: PaddingValues) {
+
+
+    val currentUser = FirebaseAuth.getInstance().currentUser
+    val userId = currentUser?.uid
+    val firestore = Firebase.firestore
+    var familyId by remember { mutableStateOf<String?>(null) }
+
+    // Firestore'dan familyId'yi al
+    LaunchedEffect(Unit) {
+        userId?.let {
+            firestore.collection("UsersTest")
+                .document(it)
+                .get()
+                .addOnSuccessListener { document ->
+                    familyId = document.getString("familyId")
+                    Log.d("RoomScreen", "FamilyID: $familyId")
+                }
+                .addOnFailureListener {
+                    Log.e("RoomScreen", "FamilyId alınamadı", it)
+                }
+        }
+    }
+
 
 
     val number = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
