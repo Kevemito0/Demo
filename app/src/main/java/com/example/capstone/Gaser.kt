ee2ca6cfc45ea58c9c1edbfefe25e15df24c8a91
hfgaser <hfgaser@gmail.com>
2025-05-12 22:10:53 +0300

    poyraz gaser merge

--- app/src/main/java/com/example/capstone/Gaser.kt
+++ app/src/main/java/com/example/capstone/Gaser.kt
@@ -2,16 +2,51 @@ package com.example.capstone
 
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Key
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
@@ -24,12 +59,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.capstone.ui.theme.CapstoneTheme
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.Date
-import java.util.UUID
 
 data class FamilyMember(
     val id: String,
@@ -45,6 +78,10 @@ data class FamilyMember(
 fun Gaser(paddingValues: PaddingValues, navController: NavHostController) {
     var selectedTabIndex by rememberSaveable { mutableStateOf(0) }
     val tabs = listOf("Family", "Join to Family", "Invitation Code")
+
+
+
+
 
     Scaffold(
         topBar = {
@@ -542,13 +579,16 @@ fun FamilyMemberListScreen(navController: NavHostController) {
     Text("Family Members (${memberList.size})", style = MaterialTheme.typography.titleLarge)
     Spacer(Modifier.height(8.dp))
 
-    LazyColumn(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
         items(memberList, key = { it.id }) { member ->
             Card(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(),
             ) {
                 Column(modifier = Modifier.padding(4.dp)) {
@@ -558,7 +598,7 @@ fun FamilyMemberListScreen(navController: NavHostController) {
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         Text("  ${member.name} (${member.role})", fontWeight = FontWeight.Bold)
-                        if (isOwner && member.role != "admin") {
+                        if (isOwner && member.role != "admin" && member.id != currentUser?.uid) {
                             IconButton(onClick = {
                                 firestore.collection("Families")
                                     .document(familyId)
@@ -600,7 +640,13 @@ fun FamilyMemberListScreen(navController: NavHostController) {
 
                                     }
                             }) {
-                                Icon(Icons.Default.Delete, contentDescription = "Delete")
+//                                if (member.id != currentUser?.uid) {
+                                    Icon(Icons.Default.Delete, contentDescription = "Delete")
+//                                }
+//                                else{
+//                                    Icon(Icons.Default.Done, contentDescription = "Done")
+//                                }
+
                             }
                         }
                     }
